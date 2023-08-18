29c08546f9aabb38e36f6f7a2c03538b5b6a3faf
Lucas <carneirolucas88@gmail.com>
2023-08-18 14:56:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExemploLogError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Simulando um erro de conexão ao banco de dados
+                throw new Exception("Erro ao conectar ao banco de dados");
+            }
+            catch (Exception ex)
+            {
+                // Registro do erro no log (no caso, exibição no console)
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
